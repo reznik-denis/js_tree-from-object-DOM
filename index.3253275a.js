de456c1793ad4f0d3679f90c04c87e57e66ae18b
Denis Reznik <reznik.d.o12@gmail.com>
2024-06-23 03:54:22 +0300

    make build

---
+++ index.3253275a.js
@@ -0,0 +1,2 @@
+!function e(n,t){var r=document.createElement("ul");for(var a in n.append(r),t){var c=document.createElement("li");r.append(c),c.textContent=a,Object.keys(t[a]).length>0&&e(c,t[a])}}(document.querySelector("#tree"),{Drink:{Wine:{},Schnaps:{}},Fruit:{Red:{Cherry:{},Strawberry:{}},Yellow:{Banana:{},Pineapple:{}}}});
+//# sourceMappingURL=index.3253275a.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,31 @@
+"use strict";
+const food = {
+    Drink: {
+        Wine: {},
+        Schnaps: {}
+    },
+    Fruit: {
+        Red: {
+            Cherry: {},
+            Strawberry: {}
+        },
+        Yellow: {
+            Banana: {},
+            Pineapple: {}
+        }
+    }
+};
+const tree = document.querySelector("#tree");
+function createTree(element, data) {
+    const ulElem = document.createElement("ul");
+    element.append(ulElem);
+    for(const key in data){
+        const liElem = document.createElement("li");
+        ulElem.append(liElem);
+        liElem.textContent = key;
+        if (Object.keys(data[key]).length > 0) createTree(liElem, data[key]);
+    }
+}
+createTree(tree, food);
+
+//# sourceMappingURL=index.f75de5e1.js.map
